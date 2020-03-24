eb3aa3a6678f673256ad8d19cc1ad484559406a4
stone1803 <51925355+stone1803@users.noreply.github.com>
2020-03-24 10:05:00 +0700

    phuc
    phuc

--- Redux/Firebase/config.js
+++ Redux/Firebase/config.js
@@ -5,14 +5,7 @@ import "firebase/database";
 import "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCBmmr3nuh-S0zgQfhtmPO9PXzxZHKANQE",
-    authDomain: "apptuvi-phucmap.firebaseapp.com",
-    databaseURL: "https://apptuvi-phucmap.firebaseio.com",
-    projectId: "apptuvi-phucmap",
-    storageBucket: "apptuvi-phucmap.appspot.com",
-    messagingSenderId: "525691376714",
-    appId: "1:525691376714:web:0949cac63415c3afb502b3",
-    measurementId: "G-LZJTJ1YBDW"
+ 
   };
 firebase.initializeApp(firebaseConfig);
 firebase.database();
